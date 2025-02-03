f7528cb0db5f69427f36eb3e459377f8fbeab63d
haimie <haimienguyen01@gmail.com>
2025-02-03 14:55:26 -0600

    Added email field to Patient table to ensure uniqueness

--- db/models.py
+++ db/models.py
@@ -5,6 +5,7 @@ class Patient(models.Model):
     first_name = fields.CharField(max_length=50)
     last_name = fields.CharField(max_length=50)
     date_of_birth = fields.DateField()  # Stores YYYY-MM-DD format
+    email = fields.CharField(max_length=100, unique=True)  # Ensures email uniqueness
 
 class Drug(models.Model):
     id = fields.IntField(pk=True)
@@ -14,4 +15,5 @@ class Prescription(models.Model):
     id = fields.IntField(pk=True)  # Primary Key
     patient = fields.ForeignKeyField("models.Patient")  # Link to Patient
     drug = fields.ForeignKeyField("models.Drug")  # Link to Drug
+    dosage = fields.CharField(max_length=100)
     refill_date = fields.DateField(null=True)  # User inputs when insurance allows next refill
